905b88e49c9e79c016cd99fa9119d255b4dfbb08
gf04\User <sbsgameas@gmail.com>
2024-03-19 19:51:28 +0900

    no message

--- Assets/Scripts/Chc.cs
+++ Assets/Scripts/Chc.cs
@@ -1,22 +1,40 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class Chc : MonoBehaviour
 {
-    [SerializeField] float Maxtorque;
+    private CharacterController Chcontroller;
+    [SerializeField] float speed;
     [SerializeField] float MaxAngle;
+    private Vector3 moveDir;
+
+
+
+    private void Awake()
+    {
+        Chcontroller = GetComponent<CharacterController>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        float motor = Maxtorque * Input.GetAxisRaw("Vertical");
-        float angle = MaxAngle * Input.GetAxisRaw("Horizontal");
+        moving();
     }
+
+    private void moving()
+    {
+        float motor = Input.GetAxisRaw("Vertical") * speed;
+        float angle = Input.GetAxisRaw("Horizontal") * MaxAngle;
+        moveDir = new Vector3(angle, 0f, motor);
+        Chcontroller.Move(moveDir);
+    }
+
 }
